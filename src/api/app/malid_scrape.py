5f137d3b91601ebf82901955ab700b22c92ffba5
SvartaHjarta <8724275+SvartaHjarta@users.noreply.github.com>
2022-08-25 18:42:36 +0000

    updated get_weekly_menu

--- src/api/app/malid_scrape.py
+++ src/api/app/malid_scrape.py
@@ -1,35 +1,46 @@
 from bs4 import BeautifulSoup
 import requests
+import datetime
+import json
+
+URL = "https://prod-10.westeurope.logic.azure.com/workflows/661a8f1211d34ed98db39ea8c6caa52f/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=BpkZO2O35IlCXBbLQ7xzM75G0GWbx7D-enVq6I8b7F8"
+MAIN = "🍴 "
+VEGAN = "🥬 "
+SOUP = "🥣 "
+
+
+def get_todays_menu():
+    r = requests.get(URL)
+    meals = json.loads(r.text)
+
+    weekday = datetime.datetime.now()
+
+    for i in meals:
+        if i["Date"] == weekday.strftime("%Y-%m-%d"):
+            m = i
+
+    weekday = weekday.strftime("%A")
+
+    menu = [ {"day": weekday, "main": MAIN+m["Title"], "vegan": VEGAN+m["VeganMenu"], "salat": "", "soup": SOUP+m["SoupOfTheDay"]}]
+
+    return {"malid": menu}
+
 
 def get_weekly_menu():
-  r = requests.get("http://malid.ru.is")
-  soup = BeautifulSoup(r.text, 'html.parser')
-  menu = [
-    {"day": "monday", "main": "", "vegan": "", "salat": "", "soup": ""},
-    {"day": "tuesday", "main": "", "vegan": "", "salat": "", "soup": ""},
-    {"day":"wednesday", "main": "", "vegan": "", "salat": "", "soup": ""}, 
-    {"day":"thursday", "main": "", "vegan": "", "salat": "", "soup": ""},
-    {"day": "friday", "main": "", "vegan": "", "salat": "", "soup": ""}
-  ]
-  menu_titles = soup.find_all("div", class_='restaurant-menu-item')
-  menu_title_items = []
-  temp = [" "]
-  for item in menu_titles:
-    for text in item.find_all("span"):
-      if len(text.contents) != 0:
-        if len(text.contents[0]) > 2:
-          temp.append(text.contents[0])
-      if len(temp) == 5:
-        menu_title_items.append(temp)
-        temp = [" "]
-  for day, item in zip(menu, menu_title_items):
-    for key, dish in zip(day.keys(), item):
-      if key != "day":
-        day[key] = dish
-  return {"malid": menu}
+    r = requests.get(URL)
+    meals = json.loads(r.text)
+    menu = []
+
+    for meal in meals:
+        weekday = datetime.datetime.strptime(meal["Date"], "%Y-%m-%d").strftime("%A")
+        menu.append({"day": weekday, "main": MAIN+meal["Title"], "vegan": VEGAN+meal["VeganMenu"], "salat": "", "soup": SOUP+meal["SoupOfTheDay"]})
+
+    return {"malid": menu}
+
 
 def main():
-  print(get_weekly_menu())
+    print(get_weekly_menu())
+
 
 if __name__ == "__main__":
-  main()
+    main()
